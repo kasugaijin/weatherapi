b97b2fc31a2be4145e4524647202c6129df077fc
Ben <robinsonbena@gmail.com>
2022-12-24 21:03:35 -0800

    add dom manipulation function and call function

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let weatherData = {}
 let fetchUrl = 'https://api.openweathermap.org/data/2.5/weather?'
 let fetchQuery = ''
 let fetchApiKey = '&appid=c9e15910f01662c7dc0ff0169b4f4e0d'
@@ -16,6 +15,8 @@ let sky = document.getElementById('skycovervalue')
 let windSpeed = document.getElementById('windspeedvalue')
 let windDirection = document.getElementById('winddirectionvalue')
 
+submitButton.addEventListener('click', getAndDisplayWeather)
+
 async function getWeather() {
   fetch(requestUrl)
     .then(function(response) {
@@ -23,7 +24,7 @@ async function getWeather() {
     })
     .then(function(response) {
       console.log(response);
-      weatherData = response
+      addDataToDom(response);
     })
     .catch(err => {
       console.error(err);
@@ -33,15 +34,26 @@ async function getWeather() {
 }
 
 function createQuery(param) {
-  return fetchQuery = ('q=' + param.trim())
+  return fetchQuery = ('q=' + param.trim());
 }
 
 function createUrl() {
-  return requestUrl = (fetchUrl + fetchQuery + fetchApiKey)
+  return requestUrl = (fetchUrl + fetchQuery + fetchApiKey);
 }
 
-
+function addDataToDom(response) {
+  temperature.innerText = response.main.temp;
+}
 
 function getAndDisplayWeather() {
-
+  createQuery(inputValue.value);
+  createUrl();
+  getWeather();
 }
+
+// sanitize query to add & for inner spaces
+// make toggle for farenheit to work
+// by default select celsius, add celsius param to url 
+// clear button
+// write the same fetch using async await, try catch
+// create  meaningful error output
